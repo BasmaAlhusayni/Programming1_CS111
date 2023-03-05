a69349bc645dc3e25e66c9cd8a7629a9a00536d0
BasmaAlhusayni <102684864+BasmaAlhusayni@users.noreply.github.com>
2023-03-05 12:38:27 +0300

    Add files via upload

---
+++ CountingVowelsInNames.java
@@ -0,0 +1,36 @@
+package countingvowelsinnames;
+
+import java.util.Scanner;
+
+public class CountingVowelsInNames {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many names do you want to enter? ");
+        int size = input.nextInt();
+
+        String[] names = new String[size];
+        System.out.println("Enter the names: ");
+        for (int i = 0; i < size; i++) {
+            names[i] = input.next();
+        }
+
+        int totalVowels = 0;
+        for (int i = 0; i < size; i++) {
+            totalVowels += countVowels(names[i]);
+        }
+
+        System.out.println("The total vowels is: " + totalVowels);
+    }
+
+    public static int countVowels(String name) {
+        int count = 0;
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
